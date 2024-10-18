d455b7e798d8a8709f7a3f6ae689881ebaad6401
huitinglim68 <91872875+huitinglim68@users.noreply.github.com>
2024-10-18 16:27:15 +0800

    Update Methodology.py

--- pages/Methodology.py
+++ pages/Methodology.py
@@ -4,19 +4,19 @@ import streamlit as st
 st.title("Methodology")
 
 # Section for Plastic Footprint Calculator
-st.markdown("### 🏨 **HDB Price Pulse**")
+st.markdown("### **Plastic Footprint Calculator**")
 
 # Add space using HTML line break
 st.markdown("<br>", unsafe_allow_html=True)
 
 # Insert Image 1
-st.image("images/Flow Chart 1.png", caption="Flow Chart 1", use_column_width=True)
+st.image("images/Flow Chart 1.PNG", caption="Flow Chart 1", use_column_width=True)
 
 # Section for Recycling Bot
-st.markdown("### 🧞 **Ask Our Resale Genie**")
+st.markdown("### ** Recycling Bot**")
 
 # Add space using HTML line break
 st.markdown("<br>", unsafe_allow_html=True)
 
 # Insert Image 2
-st.image("images/Flow Chart 2.png", caption="Flow Chart 2", use_column_width=True)
+st.image("images/Flow Chart 2.PNG", caption="Flow Chart 2", use_column_width=True)

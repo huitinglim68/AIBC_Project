66dbfac877a1eb64b452fdb1e287623368c3133b
huitinglim68 <91872875+huitinglim68@users.noreply.github.com>
2024-10-18 16:25:23 +0800

    Update Methodology.py

--- pages/Methodology.py
+++ pages/Methodology.py
@@ -3,20 +3,20 @@ import streamlit as st
 # Set the title of the Streamlit app
 st.title("Methodology")
 
-# Section for HDB Price Pulse
+# Section for Plastic Footprint Calculator
 st.markdown("### 🏨 **HDB Price Pulse**")
 
 # Add space using HTML line break
 st.markdown("<br>", unsafe_allow_html=True)
 
 # Insert Image 1
-st.image("Images/Flow Chart 1.png", caption="Flow Chart 1", use_column_width=True)
+st.image("images/Flow Chart 1.png", caption="Flow Chart 1", use_column_width=True)
 
-# Section for Ask Our Resale Genie
+# Section for Recycling Bot
 st.markdown("### 🧞 **Ask Our Resale Genie**")
 
 # Add space using HTML line break
 st.markdown("<br>", unsafe_allow_html=True)
 
 # Insert Image 2
-st.image("Images/Flow Chart 2.png", caption="Flow Chart 2", use_column_width=True)
+st.image("images/Flow Chart 2.png", caption="Flow Chart 2", use_column_width=True)

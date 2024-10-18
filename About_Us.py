0e243455bb4f186a6d988398cbeacaccf29e00f6
huitinglim68 <91872875+huitinglim68@users.noreply.github.com>
2024-10-18 19:50:36 +0800

    Update About_Us.py

--- About_Us.py
+++ About_Us.py
@@ -87,3 +87,12 @@ st.markdown("""
   - Calculation of total annual plastic waste with an animated progress bar.
   - Metrics to display total plastic waste in a visually appealing format.
 """)
+
+st.markdown("##Disclaimer")
+st.markdown("""
+IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+Always consult with qualified professionals for accurate and personalized advice.
+""")
